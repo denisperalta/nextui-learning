82a6595eedb109c0703f4fd1a540a1df5e61308e
Denis Peralta <d3nisfp@gmail.com>
2024-03-05 20:42:06 -0300

    reuniao jonathan

--- components/Jogo.tsx
+++ components/Jogo.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Divider } from "@nextui-org/divider";
 import { Input } from "@nextui-org/input";
 import Image from "next/image";
@@ -15,9 +15,21 @@ type DetalheType = {
 
 export default function Jogo({ detalhe }: { detalhe: DetalheType }) {
   const [value, setValue] = useState("");
+  const homeInputRef = useRef();
+  const awayInputRef = useRef();
+
+  // axios.get('/user', {
+  //   params: {
+  //     ID: 12345
+  //   }
+  // })
+  // .then(function (response) {
+  //   console.log(response);
+  // })
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const numberRegex = /^(\d+|)$/;
+    // INSERIR REGEX PARA 2 CARACTERES SOMENTE
     const eventValue = (e.target as HTMLInputElement)?.value;
 
     const isValidValue = numberRegex.test(eventValue) ? eventValue : value;
@@ -27,8 +39,8 @@ export default function Jogo({ detalhe }: { detalhe: DetalheType }) {
   return (
     <>
       {/* DIVISOR DE DIA */}
-      <div className="flex flex-row gap-4">
-        <div className="flex flex-1 items-center justify-end rounded-lg bg-secondary-50 px-4 py-1">
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-9 flex flex-1 items-center  rounded-lg bg-secondary-50 px-4 py-1">
           <div className="flex w-full">
             <span>Sábado</span>
             <Divider orientation="vertical" className="mx-2" />
@@ -38,17 +50,17 @@ export default function Jogo({ detalhe }: { detalhe: DetalheType }) {
             <span className="mr-10">Peso: </span>
           </div>
         </div>
-        <div className="flex items-center justify-end rounded-lg bg-secondary-50 px-4 py-1">
+        <p className="col-span-2 flex items-center justify-center rounded-lg bg-secondary-50 px-4 py-1">
           Result. Oficial
-        </div>
-        <div className="flex items-center justify-end rounded-lg bg-secondary-50 px-4 py-1">
+        </p>
+        <p className="flex items-center justify-center rounded-lg bg-secondary-50 px-4 py-1">
           Pontos
-        </div>
+        </p>
       </div>
 
       {/* JOGO */}
-      <div className="flex">
-        <div className="flex flex-1 items-center gap-2 rounded-lg bg-secondary-50 px-4 py-1">
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-9 flex items-center gap-2 rounded-lg bg-secondary-50 px-4 py-1">
           <div className="flex w-full">
             <span>Allianz Parque</span>
             <Divider orientation="vertical" className="mx-2" />
@@ -88,7 +100,7 @@ export default function Jogo({ detalhe }: { detalhe: DetalheType }) {
             <span>PAL</span>
           </div>
         </div>
-        <div className="flex items-center gap-2">
+        <div className="col-span-2 flex items-center gap-2 place-self-center">
           <div className="w-12">
             <Input
               variant="bordered"
@@ -113,7 +125,88 @@ export default function Jogo({ detalhe }: { detalhe: DetalheType }) {
             />
           </div>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center place-self-center">
+          <div className="w-12">
+            <Input
+              variant="bordered"
+              className="text-center"
+              size="sm"
+              pattern="[0-9]*"
+              type="text"
+              value=""
+              readOnly
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-9 flex items-center gap-2 rounded-lg bg-secondary-50 px-4 py-1">
+          <div className="flex w-full">
+            <span>Allianz Parque</span>
+            <Divider orientation="vertical" className="mx-2" />
+            <span>16:00 hrs</span>
+          </div>
+          <div className="mr-4 flex w-3/4 items-center justify-end gap-2 justify-self-end">
+            <span>PAL</span>
+            <Image src={palmeirasIcon} width={32} alt="Palmeiras" />
+            <div className="w-12">
+              <Input
+                variant="bordered"
+                className="text-center"
+                size="sm"
+                pattern="[0-9]*"
+                type="text"
+                onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  handleChange(e)
+                }
+                value={value}
+              />
+            </div>
+            <span>X</span>
+            <div className="w-12">
+              <Input
+                variant="bordered"
+                className="text-center"
+                size="sm"
+                pattern="[0-9]*"
+                type="text"
+                onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  handleChange(e)
+                }
+                value={value}
+              />
+            </div>
+            <Image src={cuiabaIcon} width={32} alt="Cuiaba" />
+            <span>PAL</span>
+          </div>
+        </div>
+        <div className="col-span-2 flex items-center gap-2 place-self-center">
+          <div className="w-12">
+            <Input
+              variant="bordered"
+              className="text-center"
+              size="sm"
+              pattern="[0-9]*"
+              type="text"
+              value=""
+              readOnly
+            />
+          </div>
+          <span>X</span>
+          <div className="w-12">
+            <Input
+              variant="bordered"
+              className="text-center"
+              size="sm"
+              pattern="[0-9]*"
+              type="text"
+              value=""
+              readOnly
+            />
+          </div>
+        </div>
+        <div className="flex items-center place-self-center">
           <div className="w-12">
             <Input
               variant="bordered"
